e59bcceba39b533809ef95535346e79eae59d11e
Tipu-leads <tipu.diu.cse@gmail.com>
2024-02-22 17:24:57 +0600

    profile page update
    profile page update

---
+++ iosApp/iosApp/Component/Helper/ImagePicker.swift
@@ -0,0 +1,42 @@
+//
+//  ImagePicker.swift
+//  iosApp
+//
+//  Created by LEADS Corporation Limited on 21/2/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+}
+
--- iosApp/iosApp/Views/TabViews/profile/ProfileView.swift
+++ iosApp/iosApp/Views/TabViews/profile/ProfileView.swift
@@ -4,60 +4,59 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-            VStack(spacing: 20) {
-                // User Profile Row
-                UserProfileRow(image: "person.crop.circle.fill", userName: "Md. Momtajul Karim", accountCode: "F111", boId: "5678")
-                
-                
-                // 5 Different Rows with Titles
-                TitleRow(title: "Balances", description: "Description for Row....")
-                    .onTapGesture {
-                        // Show description for Row 1
-                        print("Description for Row 1")
+                VStack(spacing: 20) {
+                    // User Profile Row
+                    UserProfileRow(image: "person.crop.circle.fill", userName: "Md. Momtajul Karim", accountCode: "F111", boId: "5678")
+                                        
+                    // 5 Different Rows with Titles
+                    TitleRow(title: "Balances")
+                        .onTapGesture {
+                            // Show description for Row 1
+                            print("Description for Row 1")
+                        }
+                    
+                    TitleRow(title: "Position")
+                        .onTapGesture {
+                            // Show description for Row 2
+                            print("Description for Row 2")
+                        }
+                    
+                    NavigationLink(destination: HomeView()) {
+                        SimpleTitleRow(title: "Portfolio Statement")
                     }
-                
-                TitleRow(title: "Position", description: "Description for Row 2")
-                    .onTapGesture {
-                        // Show description for Row 2
-                        print("Description for Row 2")
+                    
+                    NavigationLink(destination: HomeView()) {
+                        SimpleTitleRow(title: "Ledger Statement")
                     }
-                
-                NavigationLink(destination: HomeView()) {
-                    SimpleTitleRow(title: "Portfolio Statement")
+                    
+                    // Logout Row
+                    LogoutRow()
+                    // Spacer to push content to the top
+                    Spacer()
                 }
                 
-                NavigationLink(destination: HomeView()) {
-                    SimpleTitleRow(title: "Ledger Statement")
-                }
-                
-                // Logout Row
-                LogoutRow()
-                // Spacer to push content to the top
-                Spacer()
+                .padding()
+                .navigationBarTitle("Profile", displayMode: .inline)
+                .navigationBarItems(
+                    leading:
+                        Button(action: {
+                            // Handle action for the first button
+                        }) {
+                            Image(uiImage: UIImage(named: "loading_nav_button")!)
+                        },
+                    trailing:
+                        Button(action: {
+                            // Handle action for the second button
+                        }) {
+                            Image(uiImage: UIImage(named: "alarm_nav_button")!)
+                            
+                        }
+                )
             }
-            
-            .padding()
-            .navigationBarTitle("Profile", displayMode: .inline)
-            .navigationBarItems(
-                leading:
-                    Button(action: {
-                        // Handle action for the first button
-                    }) {
-                        Image(uiImage: UIImage(named: "loading_nav_button")!)
-                    },
-                trailing:
-                    Button(action: {
-                        // Handle action for the second button
-                    }) {
-                        Image(uiImage: UIImage(named: "alarm_nav_button")!)
-                        
-                    }
-            )
         }
     }
-    }
 }
-
+/*
 struct UserProfileRow: View {
     var image: String
     var userName: String
@@ -87,35 +86,55 @@ struct UserProfileRow: View {
         .shadow(radius: 5)
     }
 }
-/*
- struct TitleRow: View {
- var title: String
- var description: String
- 
- var body: some View {
- HStack {
- Text(title)
- .font(.headline)
- Spacer()
- Image(systemName: "chevron.down")
- }
- .padding()
- .background(Color.white)
- .cornerRadius(10)
- .shadow(radius: 5)
- .onTapGesture {
- // Handle tap action for other rows if needed
- print(description)
- }
- }
- }
- */
-
-
+*/
+struct UserProfileRow: View {
+    var image: String
+    var userName: String
+    var accountCode: String
+    var boId: String
+    
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerPresented = false
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                self.isImagePickerPresented.toggle()
+            }) {
+                if let selectedImage = selectedImage {
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                } else {
+                    Image(systemName: image)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+            }
+            .sheet(isPresented: $isImagePickerPresented) {
+                ImagePicker(selectedImage: $selectedImage)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(userName)
+                    .font(.headline)
+                
+                Text("Account Code: \(accountCode)")
+                Text("BO ID: \(boId)")
+                    .font(.subheadline)
+            }
+            Spacer()
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .padding(.top, 20)
+        .shadow(radius: 5)
+    }
+}
 
 struct TitleRow: View {
     var title: String
-    var description: String
     
     @State private var isExpanded: Bool = false
     
@@ -136,44 +155,61 @@ struct TitleRow: View {
             }
             
             if isExpanded {
-                // Description
-                Text(description)
-                    .padding()
                 
                 // Additional rows with data
-                HStack {
-                    Text("Available Balance")
-                    Spacer()
-                    Text("Row 2")
-                }
-                ColoredDivider(color: .black, height: 1)
-                
-                HStack {
-                    Text("Current Balance")
-                    Spacer()
-                    Text("Row 2")
-                }
-                ColoredDivider(color: .black, height: 1)
-                
-                HStack {
-                    Text("Equity")
-                    Spacer()
-                    Text("Row 2")
-                }
-                ColoredDivider(color: .black, height: 1)
-                
-                HStack {
-                    Text("Purchase Power")
-                    Spacer()
-                    Text("Row 2")
+                VStack(alignment: .leading, spacing: 10) {
+                    if title == "Balances" {
+                        additionalBalanceRow(title: "Available Balance", value: "500.00")
+                        additionalBalanceRow(title: "Current Balance", value: "500.00")
+                        additionalBalanceRow(title: "Equity", value: "500.00")
+                        additionalBalanceRow(title: "Purchase Power", value: "500.00")
+                    } else if title == "Position" {
+                        additionalPositionRow(name: "ACI", marketPrice: "500.00", averagePrice: "500.00")
+                        additionalPositionRow(name: "ACME", marketPrice: "500.00", averagePrice: "500.00")
+                    }
                 }
             }
         }
     }
+    
+    // Additional row for balance with title and value
+    private func additionalBalanceRow(title: String, value: String) -> some View {
+        HStack {
+            Text(title)
+                .font(.subheadline)
+            Spacer()
+            Text(value)
+                .font(.subheadline)
+        }
+        .padding([.leading, .trailing, .top])
+    }
+    
+    // Additional row for position with name, market price, and average price
+    private func additionalPositionRow(name: String, marketPrice: String, averagePrice: String) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(name)
+                Text("Market Price")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("Average Price")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+            VStack(alignment: .trailing) {
+                //Text(marketPrice)
+                Text(marketPrice)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text(averagePrice)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding([.leading, .trailing,.top])
+    }
 }
-
-
-
 
 
 struct SimpleTitleRow: View {
