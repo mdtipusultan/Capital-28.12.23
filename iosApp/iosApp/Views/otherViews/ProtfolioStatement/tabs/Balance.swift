aa6d0bd25011472c53fc71910940a9f89e469896
Tipu-leads <tipu.diu.cse@gmail.com>
2024-04-09 16:09:05 +0600

    balance, possition ,recivable update
    balance, possition ,recivable update

---
+++ iosApp/iosApp/Views/otherViews/ProtfolioStatement/tabs/Balance.swift
@@ -0,0 +1,19 @@
+//
+//  Balance.swift
+//  iosApp
+//
+//  Created by LEADS Corporation Limited on 9/4/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct Balance: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Balance()
+}
---
+++ iosApp/iosApp/Views/otherViews/ProtfolioStatement/tabs/Receivable.swift
@@ -0,0 +1,19 @@
+//
+//  Receivable.swift
+//  iosApp
+//
+//  Created by LEADS Corporation Limited on 9/4/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct Receivable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Receivable()
+}
