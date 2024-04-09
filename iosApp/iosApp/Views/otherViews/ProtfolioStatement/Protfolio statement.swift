edb1d179d8ce6b0f0f71091025c72a13e482cb5c
Tipu-leads <tipu.diu.cse@gmail.com>
2024-04-09 16:08:31 +0600

    protfolio statement update
    protfolio statement update

--- iosApp/iosApp/Views/otherViews/ProtfolioStatement/Protfolio statement.swift
+++ iosApp/iosApp/Views/otherViews/ProtfolioStatement/Protfolio statement.swift
@@ -1,51 +1,64 @@
 import SwiftUI
+
 struct Protfolio_statement: View {
     @State private var selectedTabIndex = 0
     let names = ["Position", "Balance", "Receivable"]
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(0..<names.count) { index in
-                            Button(action: {
-                                self.selectedTabIndex = index
-                            }) {
-                                VStack {
-                                    Text(self.names[index])
-                                        .padding()
-                                        .foregroundColor(self.selectedTabIndex == index ? .white : Color.white.opacity(0.5))
-                                    if self.selectedTabIndex == index {
-                                        Rectangle()
-                                            .frame(width: UIScreen.main.bounds.width / 3, height: 3)
-                                            .foregroundColor(.white)
-
-                                    }
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<names.count) { index in
+                        Button(action: {
+                            self.selectedTabIndex = index
+                        }) {
+                            VStack {
+                                Text(self.names[index])
+                                    .padding()
+                                    .foregroundColor(self.selectedTabIndex == index ? .white : Color.white.opacity(0.5))
+                                if self.selectedTabIndex == index {
+                                    Rectangle()
+                                        .frame(width: UIScreen.main.bounds.width / 3, height: 3)
+                                        .foregroundColor(.white)
                                 }
-                                .frame(width: UIScreen.main.bounds.width/3, height: 68)
-                                
                             }
+                            .frame(width: UIScreen.main.bounds.width/3, height: 68)
                         }
                     }
-                    .background(appColor)
                 }
-                
-                // Placeholder for content
-                switch selectedTabIndex {
-                case 0:
-                    OverviewView()
-                case 1:
-                    IndicesView()
-                case 2:
-                    StocksView()
-                default:
-                    Text("No Content")
-                }
-                
-                Spacer() // Add space to push buttons to the bottom
+                .background(appColor)
             }
+            
+            // Placeholder for content
+            switch selectedTabIndex {
+            case 0:
+                positions()
+            case 1:
+                Balance()
+            case 2:
+                Receivable()
+            default:
+                Text("No Content")
+            }
+            
+            Spacer() // Add space to push buttons to the bottom
         }
+        .navigationBarTitle(Text("Protfolio Statement"), displayMode: .inline)
+        .navigationBarItems(
+            trailing:
+            HStack(spacing: 16) { // Stack for buttons on the right
+                Button(action: {
+                    // Action for first button
+                }) {
+                    Image(uiImage: UIImage(named: "loading_nav_button")!)
+                }
+                Button(action: {
+                    // Action for second button
+                }) {
+                    Image(uiImage: UIImage(named: "alarm_nav_button")!)
+                }
+            }
+        )
     }
 }
 
